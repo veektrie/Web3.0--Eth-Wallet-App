1ee073f0d8c3bc984d8c4ac198002ece04f7d9bc
veektrie <102025110+veektrie@users.noreply.github.com>
2022-09-14 10:46:27 +0100

    nav bar created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,18 +6,17 @@ import {
   Services,
   Loader,
 } from "./components";
-const App = () => {
-  return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Welcome />
-      </div>
-      <Services />
-      <Transactions />
-      <Footer />
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  );
-};
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
 export default App;
