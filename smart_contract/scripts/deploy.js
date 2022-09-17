a6c05786f387a3d96e09964da0aeec89163a1529
veektrie <102025110+veektrie@users.noreply.github.com>
2022-09-17 19:12:58 +0100

    hardhat config updated

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -11,7 +11,7 @@ const main = async() => {
   const Transactions = await hre.ethers.getContractFactory("Transactions");
   const transactions = await Transactions.deploy();
 
-  await transactions.deployed();
+  await transactions.deployed(); 
 
   console.log(
    " Transactions deployed to: " ,transactions.address);
@@ -27,6 +27,6 @@ const runMain=async() => {
    console.error(error);
    process.exit(1);
  }
-};
+}
 
 runMain();
