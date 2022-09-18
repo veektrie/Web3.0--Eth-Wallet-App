c105bce36cd91931f2034e67b4f69432bc556341
veektrie <102025110+veektrie@users.noreply.github.com>
2022-09-18 20:29:22 +0100

    ethereum card created

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,6 +4,20 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from "./";
 
+const commonStyles =
+  'const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-white-400 text-sm font-light text-white';
+
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
+
 const Welcome = () => {
   const connectWallet = () => {};
   return (
@@ -24,6 +38,59 @@ const Welcome = () => {
           >
             <p className="text-white text-base font-semibold">Connect Wallet</p>
           </button>
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div className={"rounded-tl 4xl ${commonStyles}"}>Reliablity</div>
+            <div className={commonStyles}>Security</div>
+            <div className={"rounded-tr 4xl ${commonStyles}"}>Ethereum</div>
+            <div className={"rounded-bl 4xl ${commonStyles}"}>Web3.0</div>
+            <div className={commonStyles}>Low fees</div>
+            <div className={"rounded-br 4xl ${commonStyles}"}>Blockchain</div>
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={22} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-align blue-glassmorphism">
+            <input
+              placeholder="Address To"
+              name="addressTo"
+              type="text"
+              handleChange={() => {}}
+            />
+            <input
+              placeholder="Amount (ETH)"
+              name="amount"
+              type="number"
+              handleChange={() => {}}
+            />
+            <input
+              placeholder="Keyword(Gif)"
+              name="keyword"
+              type="text"
+              handleChange={() => {}}
+            />
+            <input
+              placeholder="Enter Message"
+              name="message"
+              type="text"
+              handleChange={() => {}}
+            />
+            <div className="h-[1px] w-full bg-gray-400 my-2" />
+          </div>
         </div>
       </div>
     </div>
