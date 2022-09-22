d4e659c4fb2e11ae7b8190167f82c194f5d36d15
veektrie <102025110+veektrie@users.noreply.github.com>
2022-09-22 12:27:25 +0100

    transaction provider created

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -23,8 +23,8 @@ const Welcome = () => {
   const handleSubmit = () => {};
   return (
     <div className="flex w-full justify-center items-center ">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-5xl sm:text-8xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
